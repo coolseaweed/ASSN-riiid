788df31c84506aef98d1043a9adafe496b3feed9
Tom Kim <37798259+coolseaweed@users.noreply.github.com>
2024-04-09 01:34:37 +0900

    Feature/wrap-up-train (#5)
    * remove command in docker-compose and add in Dockerfile
    
    * add README.md documentations
    
    * refactoring train.py to add LabelEncoder & OneHotEncoder

--- README.md
+++ README.md
@@ -1,12 +1,37 @@
-# riiid_project
-Riiid project
+# Riiid problem 3 
 
+## 1. Training
+### 1-1. 학습 모델
+학습에 사용된 모델은 `LogisticRegression` 모델이며, 테이블 형태의 데이터를 classification 할때 쉽게 적용해볼 수 있을 것 같다고 판단했습니다.
 
-# Train
-## Env setup
-
-## Run training
+### 1-2. 환경셋업
+```
+docker compose --profile train up -d --build 
+```
+### 1-3. EDA 및 학습
+EDA 및 학습 과정은 [`./train/train.ipynb`](./train/train.ipynb) 파일을 통해 확인하실 수 있으며, 간단하게 아래 커맨드를 통해 모델 학습을 할수 있습니다.
 ```bash
-docker compose --profile train up -d 
 docker exec riiid-p3-train-1 python train.py [--overwrite]
+```
+학습된 모델 weight 들은 다음과 같습니다.
+```bash
+./models/
+├── label_encoder.pkl
+├── model.pkl
+└── one_hot_encoder.pkl
+```
+### 1-4. 성능 평가
+주어진 데이터의 **30%** 를 테스트 셋으로 분리 후 학습했으며, **학습 셋**과 **테스트 셋**에 대한 정확도는 각각 **71%**, **66%** 로 측정되었습니다. 
+|Adaptive Level|precision|recall|fi-score|
+|:----|:---:|:---:|:----:|
+|High|0.40|0.67|0.50|
+|Moderate|0.67|0.66|0.66|
+|Low|0.73|0.65|0.69|
+
+## 2. Serving
+### 2-1. 서빙 아키텍처
+
+### 2-2. 환경 셋업
+```
+docker compose --profile train up -d --build 
 ```
--- train/train.py
+++ train/train.py
@@ -1,29 +1,14 @@
-import os
 from pathlib import Path
 import pickle
-from typing import Union, Tuple
+from typing import Tuple
 import argparse
-
-
-# library for feature engineering and EDA
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-# library for sampling
-from imblearn.combine import SMOTEENN
-
-# library for ML
-import sklearn
-import sklearn.linear_model
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
-from sklearn.utils.class_weight import compute_sample_weight
-from sklearn.metrics import roc_auc_score, classification_report
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 from sklearn.linear_model import LogisticRegression
-
-
+from sklearn.compose import ColumnTransformer
 
 
 def get_args():
@@ -31,48 +16,26 @@ def get_args():
     parser.add_argument(
         "-i",
         "--input",
-        default="data.csv",
+        default="/data/data.csv",
         help="train data path",
     )
     parser.add_argument(
         "-o",
         "--output",
-        default="model.save",
-        help="output model path",
-    )
-
-    parser.add_argument(
-        "--model-dir",
         default="/models",
         help="output model directory",
     )
-
-    parser.add_argument(
-        "--data-dir",
-        default="/data",
-        help="data directory",
-    )
-
     parser.add_argument(
         "--seed",
         default=1111,
         type=int,
         help="random seed",
     )
-    
-    parser.add_argument(
-        "--target-column",
-        default="Adaptivity Level",
-        type=str,
-        help="random seed",
-    )
-
     parser.add_argument(
         "--overwrite",
         action="store_true",
-        help="random seed",
+        help="overwrite models",
     )
-
 
     return parser.parse_args()
 
@@ -85,22 +48,18 @@ def data_loading(fpath: Path) -> pd.DataFrame:
     return pd.read_csv(fpath)
 
 
-def data_preprocessing(
-    df: pd.DataFrame, target_column: str, test_size: float = 0.3, seed: int = 1
-) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+def data_preprocessing(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
 
+    def text_norm(text):
+        return text.lower().replace(" ", "_").replace("-", "_")
+
+    df.columns = list(map(text_norm, df.columns))
+
+    target_column = "adaptivity_level"
     Y = df[target_column]
     X = df.drop([target_column], axis=1)
 
-    label_encoder = LabelEncoder()
-    Y_enc = label_encoder.fit_transform(Y)  #
-    X_enc = pd.get_dummies(X)  # one-hot encoding
-
-    X_train, X_test, Y_train, Y_test = train_test_split(
-        X_enc, Y_enc, test_size=test_size, random_state=seed
-    )
-
-    return X_train, X_test, Y_train, Y_test
+    return X, Y
 
 
 def model_training(
@@ -109,8 +68,8 @@ def model_training(
     Y_train: pd.DataFrame,
     Y_val: pd.DataFrame,
     seed: int = 1,
-    n_jobs:int=2
-):
+    n_jobs: int = 2,
+) -> LogisticRegression:
 
     model = LogisticRegression(
         C=1,
@@ -126,45 +85,68 @@ def model_training(
 
     return model
 
-def verify_model_performance(model:LogisticRegression,X:pd.DataFrame,Y:pd.DataFrame):
+
+def verify_model_performance(model: LogisticRegression, X: pd.DataFrame, Y: pd.DataFrame):
     X_pred = model.predict(X)
     print(f"Metric:\n{classification_report(Y,X_pred)}")
-    
-    
-def save_model(model:LogisticRegression,fpath:Path, overwrite=True):
-    
+
+
+def save_model(model, fpath: Path, overwrite=True):
+
     flag = True if not fpath.exists() else False
-    
+
     if overwrite:
         flag = True
-    
-    if flag :
-        with open(fpath,'wb') as f:
+
+    if flag:
+        with open(fpath, "wb") as f:
             pickle.dump(model, f)
-        print(f"success save model to '{fpath}'")
+        print(f"success to save model: '{fpath}'")
     else:
         raise FileExistsError(fpath)
 
+
 def main():
     args = get_args()
-    data_fpath = Path(args.data_dir) / args.input
-    model_fpath = Path(args.model_dir) / args.output
+    data_fpath = Path(args.input)
+    model_fpath = Path(args.output) / "model.pkl"
+    one_hot_encoder_fpath = Path(args.output) / "one_hot_encoder.pkl"
+    label_encoder_fpath = Path(args.output) / "label_encoder.pkl"
 
     # data loading
     df = data_loading(data_fpath)
 
     # data preprocessing
-    X_train, X_val, Y_train, Y_val = data_preprocessing(df,args.target_column)
+    X, Y = data_preprocessing(df)
+
+    ## label encoding (Y)
+    label_encoder = LabelEncoder()
+    Y_enc = label_encoder.fit_transform(Y)
+
+    ## one-hot encoding (X)
+    one_hot_encoder = OneHotEncoder(sparse_output=False)
+    ohe = ColumnTransformer(
+        transformers=[("ohe", one_hot_encoder, X.columns)],
+        remainder="passthrough",
+    )
+    ohe.set_output(transform="pandas")
+    ohe.fit(X)
+    X_enc = ohe.transform(X)
+    X_enc.head()
+
+    X_train, X_val, Y_train, Y_val = train_test_split(X_enc, Y_enc, test_size=0.3, random_state=args.seed)
 
     # model training
-    model = model_training(X_train, X_val,Y_train,Y_val)
+    model = model_training(X_train, X_val, Y_train, Y_val)
 
     # model performance verification
-    verify_model_performance(model, X_val,Y_val)
+    verify_model_performance(model, X_val, Y_val)
 
-    # save model
-    save_model(model,model_fpath,overwrite=args.overwrite)
-    
+    # save models
+    save_model(model, model_fpath, overwrite=args.overwrite)
+    save_model(label_encoder, label_encoder_fpath, overwrite=args.overwrite)
+    save_model(one_hot_encoder, one_hot_encoder_fpath, overwrite=args.overwrite)
+
 
 if __name__ == "__main__":
     main()
